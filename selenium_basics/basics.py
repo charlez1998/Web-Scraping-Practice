57b52183c417a5d1a2371c62e6740672c1d00b84
charlez1998 <charlesjacobtan1@gmail.com>
2021-06-03 17:40:52 +0800

    updated livecoin scraper to incorporate selenium with splash

--- selenium_basics/basics.py
+++ selenium_basics/basics.py
@@ -11,7 +11,7 @@ chrome_path = which("chromedriver")
 driver = webdriver.Chrome(executable_path = chrome_path, options = chrome_options)
 driver.get("https://duckduckgo.com")
 
-search_input = driver.find_element_by_id("search_form_input_homepage")
+search_input = driver.find_element_by_xpath("(//input[contains(@class , 'js-search-input')])[1]")
 search_input.send_keys("My User Agent")
 
 # search_btn = driver.find_element_by_id("search_button_homepage")
@@ -21,4 +21,4 @@ search_input.send_keys(Keys.ENTER)
 
 print(driver.page_source)
 
-# driver.close()
+driver.close()

3245137283b0a14ad00c809c0ab6146c6fe20254
charlez1998 <charlesjacobtan1@gmail.com>
2021-06-08 02:01:02 +0800

    updated silkdeals spider to yield computer deals details

--- silkdeals/silkdeals/spiders/computerdeals.py
+++ silkdeals/silkdeals/spiders/computerdeals.py
@@ -1,10 +1,22 @@
 import scrapy
-
+from scrapy_selenium import SeleniumRequest
 
 class ComputerdealsSpider(scrapy.Spider):
     name = 'computerdeals'
-    allowed_domains = ['https://slickdeals.net/computer-deals/']
-    start_urls = ['http://https://slickdeals.net/computer-deals//']
+    
+    def start_requests(self):
+        yield SeleniumRequest (
+            url = "https://slickdeals.net/computer-deals",
+            wait_time = 3,
+            callback = self.parse, 
+        )
 
     def parse(self, response):
-        pass
+        products = response.xpath('//ul[@class = "dealTiles categoryGridDeals"]/li')
+        for product in products:
+            yield {
+                'name' : product.xpath(".//a[@class = 'itemTitle bp-p-dealLink bp-c-link']/text()").get(),
+                'link' : product.xpath(".//a[@class = 'itemTitle bp-p-dealLink bp-c-link']/text()").get(),
+                'store_name' : product.xpath("normalize-space(.//span[@class = 'itemStore bp-p-storeLink bp-c-link']/text())").get(),
+                'price' : product.xpath("normalize-space(.//div[@class = 'itemPrice  wide ']/text())").get()
+            }

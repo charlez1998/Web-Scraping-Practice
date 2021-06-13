5f19db169b3efdf08effbb3b5935cd6c76ed3699
charlez1998 <charlesjacobtan1@gmail.com>
2021-06-13 17:16:06 +0800

    set up pipeline and connected to mongodb

--- imdb/imdb/pipelines.py
+++ imdb/imdb/pipelines.py
@@ -2,12 +2,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
+import pymongo
 
+class MongodbPipeline(object):
+    collection_name = "best_movies"
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+    def open_spider(self,spider):
+        self.client = pymongo.MongoClient("mongodb+srv://charlie:testtest@cluster0.prqwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        self.db = self.client["IMDB"]
 
+    def close_spider(self,spider):
+        self.client.close()
 
-class ImdbPipeline:
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item

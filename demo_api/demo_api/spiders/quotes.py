84a208543ed8da0f6a198e975973a7985f276f80
charlez1998 <charlesjacobtan1@gmail.com>
2021-06-19 15:11:04 +0800

    edit spider to include pagination

--- demo_api/demo_api/spiders/quotes.py
+++ demo_api/demo_api/spiders/quotes.py
@@ -15,3 +15,11 @@ class QuotesSpider(scrapy.Spider):
                 'tags' : quote.get('tags'),
                 'quote_text' : quote.get('text')
             }
+
+        has_next = resp.get("has_next")
+        if has_next:
+            next_page_number = resp.get('page') + 1
+            yield scrapy.Request(
+                url = f'https://quotes.toscrape.com/api/quotes?page={next_page_number}', 
+                callback = self.parse
+            )

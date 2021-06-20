6d126d2dd323ae2cc839ff5e7184612a94b216d6
charlez1998 <charlesjacobtan1@gmail.com>
2021-06-20 19:43:58 +0800

    add pagination for scraping ebooks until maximum page

--- demo_api/demo_api/spiders/ebooks.py
+++ demo_api/demo_api/spiders/ebooks.py
@@ -1,12 +1,21 @@
 import scrapy
+from scrapy.exceptions import CloseSpider
 import json 
 
 class EbooksSpider(scrapy.Spider):
     name = 'ebooks'
+
+    INCREMENTED_BY = 12
+    offset = 0 
+
     allowed_domains = ['openlibrary.org']
-    start_urls = ['http://openlibrary.org/subjects/picture_books.json?limit=12&offset=12/']
+    start_urls = ['http://openlibrary.org/subjects/picture_books.json?limit=12']
 
     def parse(self, response):
+
+        if response.status == 500:
+            raise CloseSpider('Reached last page...')
+            
         resp = json.loads(response.body)
         ebooks = resp.get('works')
         for ebook in ebooks:
@@ -14,3 +23,9 @@ class EbooksSpider(scrapy.Spider):
                 'title': ebook.get('title'),
                 'subject' : ebook.get('subject')
             }
+
+        self.offset += self.INCREMENTED_BY
+        yield scrapy.Request(
+            url = f'http://openlibrary.org/subjects/picture_books.json?limit=12&offset={self.offset}',
+            callback = self.parse
+        )

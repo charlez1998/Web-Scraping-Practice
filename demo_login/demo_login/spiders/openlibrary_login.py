bc6bcd680f34b6e4ad5024356b647e98df12df85
charlez1998 <charlesjacobtan1@gmail.com>
2021-06-24 16:03:20 +0800

    add new spider for openlibrary

---
+++ demo_login/demo_login/spiders/openlibrary_login.py
@@ -0,0 +1,24 @@
+import scrapy
+from scrapy import FormRequest
+
+class OpenlibraryLoginSpider(scrapy.Spider):
+    name = 'openlibrary_login'
+    allowed_domains = ['openlibrary.org']
+    start_urls = ['https://openlibrary.org/account/login']
+
+    def parse(self, response):
+        yield FormRequest.from_response(
+            response,
+            form_id = 'register',
+            formdata ={
+                'username': 'charlesjacobtan1@gmail.com',
+                'password' : 'test123',
+                'redirect' : '/',
+                'debug_token' : '',
+                'login' : 'Log In'
+            },
+            callback = self.after_login
+        )
+    def after_login(self,response):
+        print('Logged In...')
+        
